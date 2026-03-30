6f076659e7449e52666017ef174c42e2f7318eda
soumadeb7 <soumadebkarmakar@gmail.com>
2026-03-30 18:33:31 +0530

    add config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,20 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+  experimental: {
+    esmExternals: true,
+    isrMemoryCacheSize: 52 * 1024 * 1024,
+  },
+  outputFileTracing: true,
 };
 
 export default nextConfig;
